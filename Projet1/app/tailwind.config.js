a848e107c3a919f693409d9b1e60a1f8d9102541
--get <cedic2005@gmail.com>
2025-02-03 11:35:20 +0100

    feat(p1): Add navigation bar

--- Projet1/app/tailwind.config.js
+++ Projet1/app/tailwind.config.js
@@ -2,7 +2,7 @@
 import colors from "tailwindcss/colors";
 
 module.exports = {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    content: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: 'media',
     theme: {},
     variants: {},
