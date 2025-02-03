3f8c189de28f6d66f29df5c02988b4cb98662e71
--get <cedic2005@gmail.com>
2025-02-03 10:58:32 +0100

    feat(p1): Add tailwind

---
+++ Projet1/app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
+module.exports = {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: 'media',
+    theme: {},
+    variants: {},
+    plugins: [],
+}
