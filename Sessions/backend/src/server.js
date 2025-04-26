f89a9b3499ddb2ce19f79dde4f4ea5f02ae2b172
lynn2910 <cedric.colin35@gmail.com>
2025-04-26 23:05:22 +0200

    fix(Sessions): Make sessions persistent

--- Sessions/backend/src/server.js
+++ Sessions/backend/src/server.js
@@ -31,7 +31,8 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production'
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 7 * 24 * 60 * 60 * 1000
     }
 }));
 
